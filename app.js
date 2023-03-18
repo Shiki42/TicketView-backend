deb4ff1f2540fe3d9f2164f47234dc1d6ba9ba5c
Shuyuan <kukutou_mikiya@outlook.com>
2023-03-17 19:18:58 -0700

    feat: add CORS rule

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+const cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -22,11 +23,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/events', eventSearchRouter);
-app.use('/venue', venueSearchRouter);
+app.use('/venues', venueSearchRouter);
 app.use('/autoComplete', autoCompleteRouter);
 
 // catch 404 and forward to error handler
